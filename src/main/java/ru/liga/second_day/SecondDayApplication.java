77ac8573acfe3e2d1f2c93188150c9dbd9978aab
dvmalkin <dvmalkin@phoenixit.ru>
2023-09-11 13:41:54 +0300

    Инициализация

---
+++ src/main/java/ru/liga/second_day/SecondDayApplication.java
@@ -0,0 +1,13 @@
+package ru.liga.second_day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondDayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondDayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/liga/second_day/SecondDayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.liga.second_day;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondDayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
