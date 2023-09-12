0708add999248baebcdc83459b556c767b09a412
dvmalkin <dvmalkin@phoenixit.ru>
2023-09-12 10:41:02 +0300

    Настроил все модули, на запуск + есть настроил актуатор.

--- kitchen/src/main/java/ru/liga/Kitchen.java
+++ kitchen/src/main/java/ru/liga/Kitchen.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderService
+public class Kitchen
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(OrderService.class, args);
+        SpringApplication.run(Kitchen.class, args);
     }
 }
